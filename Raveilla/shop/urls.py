c035bf5ca7db3a1d93ca7a184120e0e8822400cd
Jhon Michael Molina <jmmmolinathebest@gmail.com>
2024-05-07 12:22:15 +0800

    saved

--- Raveilla/shop/urls.py
+++ Raveilla/shop/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
     path("men", views.men, name="men"),
     path("kids", views.kids, name="kids"),
     path("cart", views.cart, name="cart"),
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.logoutUser, name="logout"),
+
+
 
 
 
--- Raveilla/shop/views.py
+++ Raveilla/shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import redirect
 from .models import Products, WomenProducts, MensProducts, KidsProducts, Cart
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 
 
 def home(request):
+    print(request.user.username)
+    print(request.user, request.user.is_authenticated)
     data = Products.objects.all()    
     if request.method == "POST":
         title = request.POST.get("title")
@@ -13,7 +18,7 @@ def home(request):
         toAdd = Cart(title=title, price=price, description=description, image=image)
         toAdd.save()
 
-    return render(request, "home.html", {"products": data})
+    return render(request, "home.html", {"products": data, "username": request.user.username})
 def women(request):
     data = WomenProducts.objects.all()
     if request.method == "POST":
@@ -53,3 +58,30 @@ def cart(request):
 
     data = Cart.objects.all()
     return render(request, "cart.html", {"products": data})
+
+def login(request):
+    if request.method == "POST":
+        password = request.POST.get("password")
+        username = request.POST.get("username")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            return redirect("home")
+        else:
+            return render(request, "login.html", {"error": True})
+    return render(request, "login.html")
+
+
+def register(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        username = request.POST.get("username")
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return redirect("login")
+    return render(request, "register.html")
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
